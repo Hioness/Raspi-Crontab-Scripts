494d1602ccab60a114d6b2253af6ed1337e44eee
Sam <86076740+Hioness@users.noreply.github.com>
2023-04-10 10:32:06 -0400

    Add auto update

--- TV_off.sh
+++ TV_off.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Update the scripts from github repo
+cd ~/Raspi-Crontab-Scripts
+git pull https://github.com/Hioness/Raspi-Crontab-Scripts
+
 echo 'standby 0.0.0.0' | cec-client -s -d 1
 
 # echo 'on 0.0.0.0' | cec-client -s -d 1
