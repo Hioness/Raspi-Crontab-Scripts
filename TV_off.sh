996872696890cf8dc3fc7febc2559d39a7ac4675
Sam <86076740+Hioness@users.noreply.github.com>
2023-10-17 18:04:37 -0400

    Update TV_off.sh

--- TV_off.sh
+++ TV_off.sh
@@ -5,6 +5,7 @@ cd ~/Raspi-Crontab-Scripts
 git pull https://github.com/Hioness/Raspi-Crontab-Scripts
 
 echo 'standby 0.0.0.0' | cec-client -s -d 1
+echo 'standby 1.0.0.0' | cec-client -s -d 1
 
 # echo 'on 0.0.0.0' | cec-client -s -d 1
 #exec /usr/bin/chromium --kiosk
